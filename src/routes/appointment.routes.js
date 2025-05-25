c00a5f06db257c64e0c99d1f392f8cf2bb00b1fc
Mahesh <maheshchopade133@gmail.com>
2025-05-25 13:12:20 +0530

    feat: enhance appointment management with new retrieval endpoints and default status for bookings

--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,6 +1,6 @@
 import Router from "express";
-import {authMiddleware} from "../middlewares/auth.middleware.js";
-import {roleMiddleware} from "../middlewares/role.middleware.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { roleMiddleware } from "../middlewares/role.middleware.js";
 import * as appointmentController from "../controllers/appointment.controller.js";
 
 const router = Router();
@@ -14,5 +14,53 @@ router.post(
   appointmentController.bookAppointment
 );
 
+router.get(
+  "/get-appointments",
+  authMiddleware,
+  role,
+  appointmentController.getAppointments
+);
+
+router.get(
+  "/:id",
+  authMiddleware,
+  role,
+  appointmentController.getAppointmentById
+);
+
+router.put(
+  "/:id",
+  authMiddleware,
+  role,
+  appointmentController.updateAppointment
+);
+
+router.delete(
+  "/:id",
+  authMiddleware,
+  role,
+  appointmentController.deleteAppointment
+);
+
+router.get(
+  "/doctor/:doctorId",
+  authMiddleware,
+  role,
+  appointmentController.getAppointmentsByDoctorId
+);
+
+router.get(
+  "/patient/:patientId",
+  authMiddleware,
+  role,
+  appointmentController.getAppointmentsByPatientId
+);
+
+router.get(
+  "/doctor/:doctorId/patient/:patientId",
+  authMiddleware,
+  role,
+  appointmentController.getAppointmentsByPatientAndDoctor
+);
 
 export default router;
