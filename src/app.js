071e393bac8232d8d8c3f5bf81a65b30789303f6
mahesh2-lab <maheshchopade133@gmail.com>
2025-06-13 07:25:33 +0530

    Initialize Electron app and spawn backend process

--- src/app.js
+++ src/app.js
@@ -57,6 +57,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
